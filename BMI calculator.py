5f3f080c2cc077929bfb485a70890bd0e5a7ae43
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2023-06-26 12:54:35 +0530

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -2,9 +2,11 @@ h=float(input("Enter your height in meters: "))
 w=float(input("Enter your Weight in Kg: "))
  
 BMI=w/(h*h)
+
 print("BMI Calculated is:  ",BMI)
  
 if(BMI>0):
+ 
     if(BMI<=16):
         print("You are very underweight")
     elif(BMI<=18.5):
@@ -15,5 +17,6 @@ if(BMI>0):
         print("You are overweight")
     else: 
         print("You are very overweight")
+     
 else:
     print("enter valid details")
