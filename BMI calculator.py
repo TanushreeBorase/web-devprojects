46e9770c2e6b346b0609eb4aa06e43bb59d08704
Tanushree Borase <130696335+TanushreeBorase@users.noreply.github.com>
2023-06-26 12:55:06 +0530

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -9,12 +9,14 @@ if(BMI>0):
  
     if(BMI<=16):
         print("You are very underweight")
+     
     elif(BMI<=18.5):
         print("You are underweight")
     elif(BMI<=25):
         print("Congrats! You are Healthy")
     elif(BMI<=30):
         print("You are overweight")
+     
     else: 
         print("You are very overweight")
      
